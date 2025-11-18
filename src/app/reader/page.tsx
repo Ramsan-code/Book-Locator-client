36b47978d787c48f9a6cf6c2501f15c47d3d0a2c
thaventhiran Ramsan <ramsandota@gmail.com>
2025-11-18 09:22:49 +0530

    add reader page

---
+++ src/app/reader/page.tsx
@@ -0,0 +1,9 @@
+import ReaderListView from "@/section/reader/view/reader-list-view-page";
+
+export default function ReaderPage() {
+    return (
+        <div>
+           <ReaderListView />
+        </div>
+    );
+}
